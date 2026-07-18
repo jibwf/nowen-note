6c0b7ed856bfd52dc4c023f7f65c492fec408d41
Drug <cropflre2333@gmail.com>
2026-07-18 23:22:21 +0800

    fix(note-transfer): install active-workspace refresh bridge (#325)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./lib/androidCompat";
+import "./lib/noteTransferRefreshBridge";
 import "./i18n";
 // Must run before App and its import/export/editor schemas are evaluated.
 import "./lib/imageNodeTransformBootstrap";
